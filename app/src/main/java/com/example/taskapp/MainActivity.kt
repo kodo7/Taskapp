c7ad3a293e7fdbbb86f3eca4708d4a791c26a681
E <E@DESKTOP-1HCQ6PD>
2022-12-07 01:26:34 +0200

    Google authentication

--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -1,65 +1,121 @@
 package com.example.taskapp
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.Nullable
+import com.example.taskapp.databinding.ActivityMainBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    lateinit var google_img: ImageView
-
-    lateinit var gso: GoogleSignInOptions
-    lateinit var gsc: GoogleSignInClient
+    private lateinit var google_img: ImageView
+    private lateinit var register: TextView;
+    private lateinit var gso: GoogleSignInOptions
+    private lateinit var gsc: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        google_img=findViewById(R.id.google)
+        auth = Firebase.auth
+
+        //Configure Google Signin
         gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("410247991388-1h98f5o81gjn76s88rp4v8vim8h3tr4s.apps.googleusercontent.com")
             .requestEmail()
             .build()
         gsc=GoogleSignIn.getClient(this,gso)
 
-        google_img.setOnClickListener{
+        //google_img=findViewById(R.id.google)
+
+        binding.google.setOnClickListener{
                 SignIn()
         }
+        auth = FirebaseAuth.getInstance()
+        register = findViewById<TextView>(R.id.register)
+        register.setOnClickListener(this)
 
 
     }
 
     fun SignIn(){
         val intent = gsc.signInIntent;
-        startActivityForResult(intent,100)
+        startActivityForResult(intent,RC_SIGN_IN)
+    }
+    companion object{
+        const val RC_SIGN_IN = 1001
+        const val EXTRA_NAME = "EXTRA NAME"
+    }
+    override fun onClick(v:View)
+    {
+        when(v.id){
+            R.id.register -> startActivity(Intent(this, RegisterUser::class.java))
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode,resultCode,data)
+        super.onActivityResult(requestCode, resultCode, data)
 
-        if(requestCode==100){
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                task.getResult(ApiException::class.java)
-                RegisterActivity();
-            }
-            catch (e:ApiException){
-                Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
             }
         }
     }
-    fun RegisterActivity(){
-        finish();
-        val intent = Intent(applicationContext,RegisterActivity::class.java)
-        startActivity(intent)
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
     }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(applicationContext, GoogleRegisterActivity::class.java)
+            intent.putExtra(EXTRA_NAME, user.displayName)
+            startActivity(intent)
+        }
+    }
+    /*fun RegisterActivity(){
+        finish();
+        val intent = Intent(applicationContext,GoogleRegisterActivity::class.java)
+        startActivity(intent)
+    }*/
 }
